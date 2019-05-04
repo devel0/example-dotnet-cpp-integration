81b571d63f14a81c1f5b0aec699c89814ad1d5fa
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-05-04 12:31:04 +0200

    example of integration of c++ function that brings a c# function as argument

--- cpp-library/lib.cpp
+++ cpp-library/lib.cpp
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sstream>
 
 extern "C"
 {
     int sum(int a, int b)
     {
         return a + b;
+    }
+
+    void follower(int q, void *(fn)(const char *msg))
+    {
+        for (int x = 0; x < q; ++x)
+        {
+            std::stringstream ss;
+
+            ss << "sample message iteration n." << x;
+
+            fn(ss.str().c_str());
+        }
     }
 }
--- dotnet-app/Program.cs
+++ dotnet-app/Program.cs
@@ -8,9 +8,20 @@ namespace dotnet_app
         [DllImport("./lib.so")]
         public static extern int sum(int a, int b);
 
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void MyFunctionDelegate(string frame);
+
+        [DllImport("./lib.so")]
+        public static extern void follower(int q, MyFunctionDelegate fn);
+
         static void Main(string[] args)
         {
             Console.WriteLine($"1+2={sum(1, 2)}");
+
+            follower(10, (str) =>
+            {
+                System.Console.WriteLine(str);
+            });
         }
     }
 }
